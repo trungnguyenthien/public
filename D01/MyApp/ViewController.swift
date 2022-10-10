5f3c07905c907254847afa64efe7318f9ebcd61a
Trung Nguyen Thien <ngthientrung@gmail.com>
2022-10-11 06:33:45 +0700

    no message

---
+++ D01/MyApp/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  MyApp
+//
+//  Created by Trung on 01/10/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    @IBAction func aaa(_ sender: Any) {
+        print("AAA")
+    }
+}
+
